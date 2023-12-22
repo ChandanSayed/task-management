f6b24d31a2337054a3921392ed135731e8071aea
chandansayed <hangzhou635@gmail.com>
2023-12-22 18:43:27 +0600

    Another test of drag and drop

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,7 +7,8 @@ const initialTasks = [
   { id: 'task3', content: 'Prepare presentation slides for meeting', category: 'ongoing' },
   { id: 'task4', content: 'Research new marketing strategies', category: 'ongoing' },
   { id: 'task5', content: 'Finish report for Q4 sales', category: 'done' },
-  { id: 'task6', content: 'Call Sarah regarding upcoming event', category: 'done' }
+  { id: 'task6', content: 'Call Sarah regarding upcoming event', category: 'done' },
+  { id: 'task6', content: 'Call Sarah regarding upcoming event', category: 'todo' }
 ];
 
 const TodoList = () => {
@@ -33,13 +34,13 @@ const TodoList = () => {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div className="todo-list">
+      <div className="todo-list flex flex-col md:flex-row gap-10 container mx-auto p-4">
         <Droppable droppableId="ongoing">
           {provided => (
-            <div className="category" {...provided.droppableProps} ref={provided.innerRef}>
-              <h2>Ongoing</h2>
+            <div className="category bg-blue-700 text-white md:w-1/3 p-4 rounded-sm" {...provided.droppableProps} ref={provided.innerRef}>
+              <h2>Todo</h2>
               {tasks
-                .filter(task => task.category === 'ongoing')
+                .filter(task => task.category === 'todo')
                 .map((task, index) => (
                   <Draggable key={task.id} draggableId={task.id} index={index}>
                     {provided => (
@@ -54,9 +55,28 @@ const TodoList = () => {
           )}
         </Droppable>
 
+        <Droppable droppableId="ongoing">
+          {provided => (
+            <div className="category bg-blue-700 text-white md:w-1/3 p-4 rounded-sm" {...provided.droppableProps} ref={provided.innerRef}>
+              <h2>Ongoing</h2>
+              {tasks
+                .filter(task => task.category === 'done')
+                .map((task, index) => (
+                  <Draggable key={task.id} draggableId={task.id} index={index}>
+                    {provided => (
+                      <div className="task" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+                        {task.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
         <Droppable droppableId="done">
           {provided => (
-            <div className="category" {...provided.droppableProps} ref={provided.innerRef}>
+            <div className="category bg-blue-700 text-white md:w-1/3 p-4 rounded-sm" {...provided.droppableProps} ref={provided.innerRef}>
               <h2>Done</h2>
               {tasks
                 .filter(task => task.category === 'done')
