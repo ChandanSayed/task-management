8b523b4a2031a95b6e11d77f24a19d3b5403b629
chandansayed <hangzhou635@gmail.com>
2023-12-22 19:50:21 +0600

    Benifit section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Banner from './Banner';
+import TargetAudience from './TargetAudience';
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <TargetAudience />
     </>
   );
 };
---
+++ src/components/TargetAudience.jsx
@@ -0,0 +1,25 @@
+const TargetAudience = () => {
+  const audienceTypes = [
+    { id: 1, name: 'Developers', description: 'Can track the tasks of your daily life!' },
+    { id: 2, name: 'Corporate Professionals', description: 'Can track the tasks of your daily life!' },
+    { id: 3, name: 'Bankers', description: 'Can track the tasks of your daily life!' }
+  ];
+
+  return (
+    <section className="bg-gray-100 py-12">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-6">Who Can Benefit?</h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {audienceTypes.map(audience => (
+            <div key={audience.id} className="bg-white shadow-md p-6 rounded-md">
+              <h3 className="text-xl font-semibold mb-2">{audience.name}</h3>
+              <p className="text-gray-700">{audience.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TargetAudience;
