9a514ba4d6d0ef4a91da535ae200e58f688f2c47
ChandanSayed <hangzhou635@gmail.com>
2024-08-22 13:16:09 +0600

    Development Issue Fixed

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,20 +1,33 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import Loader from './Loader';
-import { Context } from '../context/AppContext';
-import { FaRegEdit } from 'react-icons/fa';
-import { MdDeleteForever } from 'react-icons/md';
-import UpdateTaskForm from './UpdateTaskForm';
-import Swal from 'sweetalert2';
-import { motion } from 'framer-motion';
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import Loader from "./Loader";
+import { Context } from "../context/AppContext";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+import UpdateTaskForm from "./UpdateTaskForm";
+import Swal from "sweetalert2";
+import { motion } from "framer-motion";
 
 const TodoList = () => {
   const [tasks, setTasks] = useState([]);
   const [initialLoading, setInitialLoading] = useState(true);
-  const { loading, setLoading, uId, allTasks, setAllTasks, updateTaskList, setUpdateTaskList } = useContext(Context);
+  const {
+    loading,
+    setLoading,
+    uId,
+    allTasks,
+    setAllTasks,
+    updateTaskList,
+    setUpdateTaskList,
+  } = useContext(Context);
   const [openPop, setOpenPop] = useState(false);
   const [updateInfo, setUpdateInfo] = useState({});
+  const boards = [
+    { id: 1, title: "To Do", category: "todo" },
+    { id: 2, title: "In Progress", category: "ongoing" },
+    { id: 3, title: "Done", category: "done" },
+  ];
 
   useEffect(() => {
     getTasksInitially();
@@ -32,23 +45,32 @@ const TodoList = () => {
 
     // const today = new Date().toISOString().split('T')[0]; international format
     const today = new Date();
-    const formattedDate = today.getFullYear() + '-' + (today.getMonth() + 1).toString().padStart(2, '0') + '-' + today.getDate().toString().padStart(2, '0');
-    const withTodayDate = await res.data.filter(list => list.deadline === formattedDate);
-    const deadlineToday = withTodayDate.filter(item => item.category !== 'done');
+    const formattedDate =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1).toString().padStart(2, "0") +
+      "-" +
+      today.getDate().toString().padStart(2, "0");
+    const withTodayDate = await res.data.filter(
+      (list) => list.deadline === formattedDate
+    );
+    const deadlineToday = withTodayDate.filter(
+      (item) => item.category !== "done"
+    );
 
     if (deadlineToday.length > 0) {
-      console.log('You have some tasks to complete today');
+      console.log("You have some tasks to complete today");
       Swal.fire({
-        title: 'Remember!',
+        title: "Remember!",
         text: `You have ${deadlineToday.length} tasks to complete today!`,
-        icon: 'info'
+        icon: "info",
       });
     }
     if (res.data.length < 1) {
       Swal.fire({
-        title: 'Remember!',
+        title: "Remember!",
         text: `You haven't added any task, why not today!`,
-        icon: 'info'
+        icon: "info",
       });
     }
   }
@@ -63,25 +85,30 @@ const TodoList = () => {
     return <Loader />;
   }
 
-  const onDragEnd = async result => {
+  const onDragEnd = async (result) => {
     const { destination, source, draggableId } = result;
 
     if (!destination) {
       return;
     }
 
-    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
       return;
     }
 
     const updatedTasks = [...tasks];
-    const task = updatedTasks.find(t => t._id === draggableId);
+    const task = updatedTasks.find((t) => t._id === draggableId);
     task.category = destination.droppableId;
-    console.log(task.category, destination.droppableId, updatedTasks);
-    // console.log(task);
-    const res = await axios.put(`/tasks/update-task-category/${task._id}`, { category: task.category });
-    // console.log(res.data);
-    setTasks(updatedTasks);
+    const res = await axios.put(`/tasks/update-task-category/${task._id}`, {
+      category: task.category,
+    });
+
+    if (res.data) {
+      setTasks(updatedTasks);
+    }
   };
 
   async function handleDelete(id) {
@@ -90,11 +117,11 @@ const TodoList = () => {
     // console.log(res.data);
     if (res.data.deletedCount) {
       Swal.fire({
-        title: 'Congrats!',
+        title: "Congrats!",
         text: `Your task Successfully deleted!`,
-        icon: 'success'
+        icon: "success",
       });
-      setUpdateTaskList(prev => prev + 1);
+      setUpdateTaskList((prev) => prev + 1);
     }
   }
 
@@ -113,132 +140,111 @@ const TodoList = () => {
           transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 }
+            visible: { opacity: 1, x: 0 },
           }}
           className="todo-list flex flex-col md:flex-row gap-10 container mx-auto p-4"
         >
-          <Droppable droppableId="todo">
-            {provided => (
-              <div className="category bg-blue-700 text-white md:w-1/3 p-4 rounded-sm" {...provided.droppableProps} ref={provided.innerRef}>
-                <h2 className="text-4xl font-bold text-yellow-400 mb-5">Todo</h2>
-                {tasks
-                  .filter(task => task.category === 'todo')
-                  .map((task, index) => (
-                    <Draggable key={task._id} draggableId={task._id} index={index}>
-                      {provided => (
-                        <div className="task mb-4" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                          <div className="flex gap-2 flex-row items-center justify-between">
-                            <div className="flex-1">
-                              <p>Task Title</p>
-                              <p>{task.title}</p>
-                            </div>
-                            <div className="flex-1">
-                              <p>Due date</p>
-                              <p>{task.deadline}</p>
-                            </div>
-                            <button onClick={() => handleUpdate(task)} className="btn ml-2 p-2 min-h-min h-auto">
-                              <FaRegEdit />
-                            </button>
-                            <button className="btn p-2 h-auto min-h-min" onClick={() => handleDelete(task._id)}>
-                              <MdDeleteForever />
-                            </button>
-                          </div>
-                          <div>
-                            <p>Task Description</p>
-                            <p>{task.description}</p>
-                          </div>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-
-          <Droppable droppableId="ongoing">
-            {provided => (
-              <div className="category bg-blue-700 text-white md:w-1/3 p-4 rounded-sm" {...provided.droppableProps} ref={provided.innerRef}>
-                <h2 className="text-4xl font-bold text-yellow-400 mb-5">Ongoing</h2>
-                {tasks
-                  .filter(task => task.category === 'ongoing')
-                  .map((task, index) => (
-                    <Draggable key={task._id} draggableId={task._id} index={index}>
-                      {provided => (
-                        <div className="task mb-4" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                          <div className="flex gap-2 flex-row items-center justify-between">
-                            <div className="flex-1">
-                              <p>Task Title</p>
-                              <p>{task.title}</p>
-                            </div>
-                            <div className="flex-1">
-                              <p>Due date</p>
-                              <p>{task.deadline}</p>
-                            </div>
-                            <button onClick={() => handleUpdate(task)} className="btn ml-2 p-2 min-h-min h-auto">
-                              <FaRegEdit />
-                            </button>
-                            <button className="btn p-2 h-auto min-h-min" onClick={() => handleDelete(task._id)}>
-                              <MdDeleteForever />
-                            </button>
-                          </div>
-                          <div className="">
-                            <p>Task description</p>
-                            <p>{task.description}</p>
-                          </div>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-          <Droppable droppableId="done">
-            {provided => (
-              <div className="category bg-blue-700 text-white md:w-1/3 p-4 rounded-sm" {...provided.droppableProps} ref={provided.innerRef}>
-                <h2 className="text-4xl font-bold text-yellow-400 mb-5">Done</h2>
-                {tasks
-                  .filter(task => task.category === 'done')
-                  .map((task, index) => (
-                    <Draggable key={task._id} draggableId={task._id} index={index}>
-                      {provided => (
-                        <div className="task mb-4" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                          <div className=" flex gap-2 flex-row items-center justify-between">
-                            <div className="flex-1">
-                              <p>Task Title</p>
-                              <p>{task.title}</p>
-                            </div>
-                            <div className="flex-1">
-                              <p>Due date</p>
-                              <p>{task.deadline}</p>
-                            </div>
-                            <button onClick={() => handleUpdate(task)} className="btn ml-2 p-2 min-h-min h-auto">
-                              <FaRegEdit />
-                            </button>
-                            <button className="btn p-2 h-auto min-h-min" onClick={() => handleDelete(task._id)}>
-                              <MdDeleteForever />
-                            </button>
-                          </div>
-                          <div className="flex-1">
-                            <p>Task Description</p>
-                            <p>{task.description}</p>
-                          </div>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
+          {boards.map((board) => {
+            return (
+              <DropComponent
+                board={board}
+                tasks={tasks}
+                key={board.id}
+                handleUpdate={handleUpdate}
+                handleDelete={handleDelete}
+              />
+            );
+          })}
         </motion.div>
       </DragDropContext>
-      <div className={`h-screen flex flex-col items-center justify-center fixed inset-0 bg-black bg-opacity-90 ${openPop ? 'block' : 'hidden'}`}>
-        <UpdateTaskForm setUpdateTaskList={setUpdateTaskList} updateInfo={updateInfo} setUpdateInfo={setUpdateInfo} openPop={openPop} setOpenPop={setOpenPop} />
+      <div
+        className={`h-screen flex flex-col items-center justify-center fixed inset-0 bg-black bg-opacity-90 ${
+          openPop ? "block" : "hidden"
+        }`}
+      >
+        <UpdateTaskForm
+          setUpdateTaskList={setUpdateTaskList}
+          updateInfo={updateInfo}
+          setUpdateInfo={setUpdateInfo}
+          openPop={openPop}
+          setOpenPop={setOpenPop}
+        />
       </div>
     </>
   );
 };
 
 export default TodoList;
+
+function DropComponent({ board, tasks, handleUpdate, handleDelete }) {
+  const [enabled, setEnabled] = useState(false);
+  useEffect(() => {
+    const animation = requestAnimationFrame(() => setEnabled(true));
+
+    return () => {
+      cancelAnimationFrame(animation);
+      setEnabled(false);
+    };
+  }, []);
+
+  if (!enabled) {
+    return null;
+  }
+  return (
+    <Droppable droppableId={board.category}>
+      {(provided) => (
+        <div
+          className="category bg-blue-700 text-white md:w-1/3 p-4 rounded-sm"
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+        >
+          <h2 className="text-4xl font-bold text-yellow-400 mb-5">
+            {board.title}
+          </h2>
+          {tasks
+            .filter((task) => task.category === board.category)
+            .map((task, index) => (
+              <Draggable key={task._id} draggableId={task._id} index={index}>
+                {(provided) => (
+                  <div
+                    className="task mb-4"
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    <div className="flex gap-2 flex-row items-center justify-between">
+                      <div className="flex-1">
+                        <p>Task Title</p>
+                        <p>{task.title}</p>
+                      </div>
+                      <div className="flex-1">
+                        <p>Due date</p>
+                        <p>{task.deadline}</p>
+                      </div>
+                      <button
+                        onClick={() => handleUpdate(task)}
+                        className="btn ml-2 p-2 min-h-min h-auto"
+                      >
+                        <FaRegEdit />
+                      </button>
+                      <button
+                        className="btn p-2 h-auto min-h-min"
+                        onClick={() => handleDelete(task._id)}
+                      >
+                        <MdDeleteForever />
+                      </button>
+                    </div>
+                    <div>
+                      <p>Task Description</p>
+                      <p>{task.description}</p>
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import AppContext from './context/AppContext.jsx';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from './components/Home.jsx';
-import Error from './components/Error.jsx';
-import PrivateRoute from './components/PrivateRoute.jsx';
-import Login from './components/Login.jsx';
-import Dashboard from './components/Dashboard.jsx';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import AppContext from "./context/AppContext.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Error from "./components/Error.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Login from "./components/Login.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://task-management-server-16zi.onrender.com';
+axios.defaults.baseURL = "https://task-management-server-16zi.onrender.com";
+// axios.defaults.baseURL = "http://localhost:5000/";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <Error />,
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: '/login',
-        element: <Login />
+        path: "/login",
+        element: <Login />,
       },
       {
-        path: '/dashboard',
+        path: "/dashboard",
         element: (
           <PrivateRoute>
             <Dashboard />
           </PrivateRoute>
-        )
-      }
-    ]
-  }
+        ),
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppContext>
       <RouterProvider router={router} />
